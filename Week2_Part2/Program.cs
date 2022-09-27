977a4964102c5f74489cf0f9c11f1fc4fe58a291
wests <wests@C-010053>
2022-09-27 14:30:42 +0100

    Added check enviro

--- Week2_Part2/Program.cs
+++ Week2_Part2/Program.cs
@@ -1,3 +1,8 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello World Again!");
 Console.WriteLine("And again...");
+
+if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+{
+    Console.WriteLine("You are using Windows!");
+}
